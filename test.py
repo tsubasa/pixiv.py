1c179abbb3ef41a905fb6f9e5a9fc028dcec52e4
tsubasa <twoopon@gmail.com>
2014-11-22 01:12:08 +0900

    テストスクリプト更新

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from api import PixivApi, PixivIllustLookup, PixivIllustSearch, PixivResultParser
+from api import PixivApi, PixivIllustLookup, PixivIllustSearch, PixivResultParser, PixivNovelSearch, PixivNovelLookup
 from config import config, args
 
 def pixiv_bot():
@@ -9,26 +9,23 @@ def pixiv_bot():
 
     # イラストルックアップ
     illust = PixivIllustLookup(api)
-    print 'raw data'
     print illust.lookup(47162152)
-    # raw data
 
-    for illust in PixivResultParser(illust.lookup(47162152)):
-        print 'illust lookup'
-        test_print(illust)
+    # 小説ルックアップ
+    novel = PixivNovelLookup(api)
+    print novel.lookup(4579009)
 
     # イラスト検索
-    for result in PixivIllustSearch(api, keyword=args.keyword, page=args.page):
+    for result in PixivIllustSearch(api, keyword='魔法少女まどか☆マギカ', page=200):
         for illust in PixivResultParser(result):
-            test_print(illust)
+            print illust
+            print 'id: %s title: %s by %s (%s)' % (illust.id, illust.title, illust.post_name, illust.user_name)
 
-def test_print(illust):
-    print illust
-    print 'id   : %s' % (illust.illust_id,)
-    print 'title: %s by %s (%s)' % (illust.title, illust.post_name, illust.user_name)
-    print 'tags : %s' % (', '.join(illust.tags),)
-    print illust.imgs
-    # object data
+    # 小説検索
+    for result in PixivNovelSearch(api, keyword='魔法少女まどか☆マギカ', page=200):
+        for novel in PixivResultParser(result):
+            print novel
+            print 'id: %s title: %s by %s (%s)' % (novel.id, novel.title, novel.post_name, novel.user_name)
 
 if __name__ == '__main__':
     pixiv_bot()
